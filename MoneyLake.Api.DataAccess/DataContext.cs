6d8b8fce0f30c1d56149f3e4bff51bc3a91e9850
Andrey Prokhorov <andrey@ezy.se>
2017-06-20 23:36:09 +0200

    12 - Added "OnConfiguring" method to DataContext
    Removed constructor according to
    https://github.com/aspnet/EntityFramework/issues/4825

--- MoneyLake.Api.DataAccess/DataContext.cs
+++ MoneyLake.Api.DataAccess/DataContext.cs
@@ -6,16 +6,19 @@ namespace MoneyLake.Api.DataAccess
     public class DataContext: DbContext
     {
         public DbSet<GreenHouse> GreenHouses { get; set; }
-        
-        public DataContext(DbContextOptions options) : base(options)
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
+            var connectionString = "Host=fizzy-cherry.db.elephantsql.com;Database=tbqctuyo;Username=tbqctuyo;Password=bsaJa6_79RsMcdghTQ3PS3DZsymEpCdx";
+            builder.UseNpgsql(connectionString);
+            base.OnConfiguring(builder);
         }
-        
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<GreenHouse>().ToTable("GreenHouses");
             modelBuilder.Entity<GreenHouse>().HasKey(x => x.Id);
-            modelBuilder.Entity<GreenHouse>().HasIndex(x => x.Id);
+            modelBuilder.Entity<GreenHouse>().HasIndex(x => x.Id);            
         }
     }
 }
