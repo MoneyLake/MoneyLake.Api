5fdad5e053b688068ddb8f4fd2478473a64aa85a
Andrey Prokhorov <andrey@ezy.se>
2017-06-20 23:34:48 +0200

    12 - Added EF migrations
    Used following commando in PM console:
    Add-Migration CreateGreenhouses -Project MoneyLake.Api.DataAccess
    -StartupProject MoneyLake.Api

---
+++ MoneyLake.Api.DataAccess/Migrations/DataContextModelSnapshot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MoneyLake.Api.DataAccess;
+
+namespace MoneyLake.Api.DataAccess.Migrations
+{
+    [DbContext(typeof(DataContext))]
+    partial class DataContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("MoneyLake.Api.DataAccess.DTO.GreenHouse", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("GreenHouses");
+                });
+        }
+    }
+}
